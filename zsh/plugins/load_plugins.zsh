e29639ce0058a542864b767b9ca551d6428fedc4
CaptainStandby <18215579+CaptainStandby@users.noreply.github.com>
2021-11-25 14:31:52 +0100

    add possibility to set options for plugins

--- zsh/plugins/load_plugins.zsh
+++ zsh/plugins/load_plugins.zsh
@@ -1,5 +1,8 @@
 
 0=${(%):-%N}
+
+set -o allexport; source "${0:A:h}/options.env"; set +o allexport
+
 find "${0:A:h}/" -type f -name '*.plugin.zsh' -print0 | while IFS= read -r -d $'\0' file; do
 	source $file
 done

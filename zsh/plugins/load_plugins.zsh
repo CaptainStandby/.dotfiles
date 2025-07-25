7a6582d1e138ac19c09b7f2d0ba03350b19b892c
CaptainStandby <18215579+CaptainStandby@users.noreply.github.com>
2025-07-25 14:26:45 +0200

    dont descend below the first level of plugins

--- zsh/plugins/load_plugins.zsh
+++ zsh/plugins/load_plugins.zsh
@@ -3,6 +3,6 @@
 
 set -o allexport; source "${0:A:h}/options.env"; set +o allexport
 
-find "${0:A:h}/" -type f -name '*.plugin.zsh' -print0 | while IFS= read -r -d $'\0' file; do
+find "${0:A:h}/" -type f -name '*.plugin.zsh' -maxdepth 2 -print0 | while IFS= read -r -d $'\0' file; do
 	source $file
 done
